a531f554ebd83dbeab2e4258a79d0c1410f890e0
madhanpillay <ajp11779@imacs-imac.ajubachn1>
2016-12-28 14:53:55 +0530

    Initial Commit

---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Madhan Masilamani on 12/28/16.
+//  Copyright © 2016 Madhan Masilamani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by Madhan Masilamani on 12/28/16.
+//  Copyright © 2016 Madhan Masilamani. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
